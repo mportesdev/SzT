b6ee67296209169e8b3de6b1cc8aabd993127c17
myrmica-habilis <michalportes1@gmail.com>
2020-01-02 19:25:38 +0100

    Create a superclass for Weapon and Consumable

--- items.py
+++ items.py
@@ -1,23 +1,26 @@
 # coding: utf-8
 
 
-class Weapon:
-    def __init__(self, name, damage, value, name_accusative=None):
+class Item:
+    def __init__(self, name, value, name_accusative=None):
         self.name = name
-        self.damage = damage
         self.value = value
         self.name_accusative = name_accusative or self.name
+
+
+class Weapon(Item):
+    def __init__(self, name, damage, value, name_accusative=None):
+        super().__init__(name, value, name_accusative)
+        self.damage = damage
 
     def __str__(self):
         return f'{self.name_accusative} (útok +{self.damage})'
 
 
-class Consumable:
+class Consumable(Item):
     def __init__(self, name, healing_value, value, name_accusative=None):
-        self.name = name
+        super().__init__(name, value, name_accusative)
         self.healing_value = healing_value
-        self.value = value
-        self.name_accusative = name_accusative or self.name
 
     def __str__(self):
         return f'{self.name_accusative} (+{self.healing_value} % zdraví)'
