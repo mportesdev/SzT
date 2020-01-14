9825e6577e64dc62cfde38778ab7bf0d6bf2bd8e
myrmica-habilis <michalportes1@gmail.com>
2020-01-14 09:47:38 +0100

    Parametrize test

--- tests.py
+++ tests.py
@@ -14,11 +14,10 @@ def item():
     return Item('name', 100)
 
 
-def test_trader_buy_price(trader, item):
-    assert trader.buy_price(item) == 90
-
-    item.value = 2
-    assert trader.buy_price(item) == 1
-
-    item.value = 12
-    assert trader.buy_price(item) == 10
+@pytest.mark.parametrize('value, result', ((1, 0), (2, 1),
+                                           (9, 8), (10, 9), (11, 9),
+                                           (99, 89), (100, 90), (101, 90),
+                                           (109, 98), (110, 99), (111, 99)))
+def test_trader_buy_price(trader, item, value, result):
+    item.value = value
+    assert trader.buy_price(item) == result
