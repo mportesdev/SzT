58d30be2d9ec70937d5b0d4a2d38f5ce1589c058
myrmica-habilis <michalportes1@gmail.com>
2020-01-19 18:58:18 +0100

    Update version and date

--- utils.py
+++ utils.py
@@ -42,7 +42,7 @@ def print_game_title():
                 '\n',
                 'textová hra na hrdiny'.center(WIDTH),
                 '',
-                'verze 0.5, 13. ledna 2020'.center(WIDTH),
+                'verze 0.6, 19. ledna 2020'.center(WIDTH),
                 '\n\n',
                 sep='\n',
                 color='1;95')
