df59f5c1a1fc6e7adc2729f3a6dcc0e26df00c6c
myrmica-habilis <michalportes1@gmail.com>
2020-07-21 13:24:26 +0200

    Construct only one Theme object

--- szt/barvy.py
+++ szt/barvy.py
@@ -5,44 +5,39 @@ import sys
 from rich.style import Style
 from rich.theme import Theme
 
-SVĚTLÉ_BARVY = Theme(
-    {
-        'červená': Style.parse('bright_red'),
-        'modrá': Style.parse('bright_blue'),
-        'fialová': Style.parse('bright_magenta'),
-        'tyrkys': Style.parse('bright_cyan'),
-    }
-)
-SVĚTLÉ_BARVY_TUČNĚ = Theme(
-    {
-        'červená': Style.parse('bold bright_red'),
-        'modrá': Style.parse('bold bright_blue'),
-        'fialová': Style.parse('bold bright_magenta'),
-        'tyrkys': Style.parse('bold bright_cyan'),
-    }
-)
-TMAVÉ_BARVY = Theme(
-    {
-        'červená': Style.parse('red'),
-        'modrá': Style.parse('blue'),
-        'fialová': Style.parse('magenta'),
-        'tyrkys': Style.parse('cyan'),
-    }
-)
-ŽÁDNÉ_BARVY = Theme(
-    {
-        'červená': Style(),
-        'modrá': Style(),
-        'fialová': Style(),
-        'tyrkys': Style(),
-    }
-)
+SVĚTLÉ_BARVY = {
+    'červená': Style.parse('bright_red'),
+    'modrá': Style.parse('bright_blue'),
+    'fialová': Style.parse('bright_magenta'),
+    'tyrkys': Style.parse('bright_cyan'),
+}
+
+TUČNÉ_SVĚTLÉ_BARVY = {
+    'červená': Style.parse('bold bright_red'),
+    'modrá': Style.parse('bold bright_blue'),
+    'fialová': Style.parse('bold bright_magenta'),
+    'tyrkys': Style.parse('bold bright_cyan'),
+}
+
+TMAVÉ_BARVY = {
+    'červená': Style.parse('red'),
+    'modrá': Style.parse('blue'),
+    'fialová': Style.parse('magenta'),
+    'tyrkys': Style.parse('cyan'),
+}
+
+ŽÁDNÉ_BARVY = {
+    'červená': Style(),
+    'modrá': Style(),
+    'fialová': Style(),
+    'tyrkys': Style(),
+}
 
 if '-B' in sys.argv[1:]:
-    barevný_motiv = ŽÁDNÉ_BARVY
+    barevný_motiv = Theme(ŽÁDNÉ_BARVY)
 elif '-T' in sys.argv[1:]:
-    barevný_motiv = TMAVÉ_BARVY
+    barevný_motiv = Theme(TMAVÉ_BARVY)
 elif sys.platform == 'win32':
-    barevný_motiv = SVĚTLÉ_BARVY_TUČNĚ
+    barevný_motiv = Theme(TUČNÉ_SVĚTLÉ_BARVY)
 else:
-    barevný_motiv = SVĚTLÉ_BARVY
+    barevný_motiv = Theme(SVĚTLÉ_BARVY)
